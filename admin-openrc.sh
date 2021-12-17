1438b2d3f577d67e6320b2fbf78a97905760142a
Randy <55771173+drexxcbba@users.noreply.github.com>
2021-12-16 22:43:29 -0400

    Update admin-openrc.sh

--- admin-openrc.sh
+++ admin-openrc.sh
@@ -10,10 +10,10 @@
 # OpenStack API is version 3. For example, your cloud provider may implement
 # Image API v1.1, Block Storage API v2, and Compute API v2.0. OS_AUTH_URL is
 # only for the Identity API served through keystone.
-export OS_AUTH_URL=http://192.168.0.21/identity
+export OS_AUTH_URL=http://192.168.0.17/identity
 # With the addition of Keystone we have standardized on the term **project**
 # as the entity that owns the resources.
-export OS_PROJECT_ID=75bdf1a5616d4098ad1aee6924fb459e
+export OS_PROJECT_ID=e8a8d0c502c44f38aa7ea832dc6fb327
 export OS_PROJECT_NAME="admin"
 export OS_USER_DOMAIN_NAME="Default"
 if [ -z "$OS_USER_DOMAIN_NAME" ]; then unset OS_USER_DOMAIN_NAME; fi
